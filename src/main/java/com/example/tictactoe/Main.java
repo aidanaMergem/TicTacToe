e36964a87eb7d8448f44c43af4bdddc41dd8c196
aidanaMergem <aidanamergembaeva@gmail.com>
2023-06-16 13:41:04 +0600

    finish

--- src/main/java/com/example/tictactoe/Main.java
+++ src/main/java/com/example/tictactoe/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Tic TacToe");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/tictactoe/UserController.java
+++ src/main/java/com/example/tictactoe/UserController.java
@@ -33,9 +33,8 @@ public class UserController {
     @FXML
     private Button btn22;
 
-
     @FXML
-    private Label Winner;
+    private Label winnerLabel;
 
 
     @FXML
@@ -47,28 +46,102 @@ public class UserController {
     @FXML
     private Label userItem;
 
-
-
-    @FXML
-    void restart(ActionEvent event) {
-
-    }
-
-    @FXML
-    void selectO(ActionEvent event) {
-
-    }
-
-    @FXML
-    void selectX(ActionEvent event) {
-
-    }
-
+    private String currentPlayer = "X";
 
     @FXML
     private void clickBtn(ActionEvent event) {
         Button clickedButton = (Button) event.getSource();
-        clickedButton.setText("X");
+        if (clickedButton.getText().isEmpty()) {
+            clickedButton.setText(currentPlayer);
+            checkWinner();
+            switchPlayer();
+        }
     }
 
+    private void switchPlayer() {
+        if (currentPlayer.equals("X")) {
+            currentPlayer = "O";
+            turn.setText("O's turn");
+        } else {
+            currentPlayer = "X";
+            turn.setText("X's turn");
+
+        }
+    }
+
+    private void checkWinner() {
+        String[][] board = {
+                {btn00.getText(), btn01.getText(), btn02.getText()},
+                {btn10.getText(), btn11.getText(), btn12.getText()},
+                {btn20.getText(), btn21.getText(), btn22.getText()}
+        };
+
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (!board[i][0].isEmpty() && board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2])) {
+                displayWinner(board[i][0]);
+                return;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (!board[0][j].isEmpty() && board[0][j].equals(board[1][j]) && board[0][j].equals(board[2][j])) {
+                displayWinner(board[0][j]);
+                return;
+            }
+        }
+
+        // Check diagonals
+        if (!board[0][0].isEmpty() && board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2])) {
+            displayWinner(board[0][0]);
+            return;
+        }
+
+        if (!board[0][2].isEmpty() && board[0][2].equals(board[1][1]) && board[0][2].equals(board[2][0])) {
+            displayWinner(board[0][2]);
+        }
+    }
+
+    private void displayWinner(String winner) {
+        winnerLabel.setText("Winner: " + winner);
+        disableButtons();
+    }
+
+    private void disableButtons() {
+        btn00.setDisable(true);
+        btn01.setDisable(true);
+        btn02.setDisable(true);
+        btn10.setDisable(true);
+        btn11.setDisable(true);
+        btn12.setDisable(true);
+        btn20.setDisable(true);
+        btn21.setDisable(true);
+        btn22.setDisable(true);
+    }
+
+    public void selectX(ActionEvent actionEvent) {
+        userItem.setText("X");
+        computerItem.setText("O");
+        currentPlayer = "X";
+    }
+
+    public void selectO(ActionEvent actionEvent) {
+        userItem.setText("O");
+        computerItem.setText("X");
+        currentPlayer ="O";
+    }
+
+
+    public void restart(ActionEvent actionEvent) {
+        btn00.setText("");
+        btn01.setText("");
+        btn02.setText("");
+        btn10.setText("");
+        btn11.setText("");
+        btn12.setText("");
+        btn20.setText("");
+        btn21.setText("");
+        btn22.setText("");
+    }
 }
